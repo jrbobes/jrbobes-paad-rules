c353d13ef194fde574c2d261f76e5ee497cbccf2
José Bobes Bascarán <jrbobes@gmail.com>
2022-05-31 02:12:46 +0200

    Added Rule Details

--- src/main/java/com/jrbobes/jrbobespaadrules/RulesController.java
+++ src/main/java/com/jrbobes/jrbobespaadrules/RulesController.java
@@ -2,13 +2,15 @@ package com.jrbobes.jrbobespaadrules;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+
 public class RulesController {
 
     @RequestMapping("/rules")
@@ -21,13 +23,13 @@ public class RulesController {
         return "rulesList";
     }
 
-    @RequestMapping("/rule")
-    public String rule(@RequestParam(name = "id", required = false) Integer id, Model model) {
+    @GetMapping("/rules/{ruleId}")
+    public String rule(@PathVariable Integer ruleId, Model model) {
 
         List<Rule> rules = new ArrayList<Rule>();
         rules.add(new Rule(1, "Primera"));
         rules.add(new Rule(2, "Segunda"));
-        model.addAttribute("rule", rules.get(id));
+        model.addAttribute("rule", rules.get(ruleId-1));
         return "rule";
     }
 
