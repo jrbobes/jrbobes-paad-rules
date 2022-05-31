3aa434962eef2dab00ea57756600617da0699a2f
José Bobes Bascarán <jrbobes@gmail.com>
2022-06-01 01:28:40 +0200

    Added Easy rules dependency and draft rule

--- src/main/java/com/jrbobes/paadrules/controller/RulesController.java
+++ src/main/java/com/jrbobes/paadrules/controller/RulesController.java
@@ -3,7 +3,6 @@ package com.jrbobes.paadrules.controller;
 import com.jrbobes.paadrules.Rule;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-
 public class RulesController {
 
     @RequestMapping("/rules")
@@ -24,13 +22,13 @@ public class RulesController {
         return "rulesList";
     }
 
-    @GetMapping("/rules/{ruleId}")
+    @RequestMapping("/rules/{ruleId}")
     public String rule(@PathVariable Integer ruleId, Model model) {
 
         List<Rule> rules = new ArrayList<Rule>();
         rules.add(new Rule(1, "Primera"));
         rules.add(new Rule(2, "Segunda"));
-        model.addAttribute("rule", rules.get(ruleId-1));
+        model.addAttribute("rule", rules.get(ruleId - 1));
         return "rule";
     }
 
